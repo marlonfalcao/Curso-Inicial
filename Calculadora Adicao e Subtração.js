608d34073c142e254a29f2e1841edaa503c2e988
Marlon <marlon.falcao@icloud.com>
2021-03-20 03:05:20 -0300

    delete do teste e adicao da calculadora

---
+++ Calculadora Adicao e Subtração.js
@@ -0,0 +1,17 @@
+var numero = 2
+var outroNumero = 2
+var operacao = ""
+var resultado = 0
+
+function calculadoraAdicaoSubtracao(numero, outroNumero, operacao) {
+    if (operacao === "+" ){
+        resultado = numero + outroNumero
+    } else if (operacao === "-"){
+        resultado = numero - outroNumero
+    } else 0
+debugger
+return resultado
+
+}
+
+console.log (calculadoraAdicaoSubtracao(20,5,"-"))
